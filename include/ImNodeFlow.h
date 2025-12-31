e821243d89aa93ac3b02cb5db00322a2deb99f34
MukulWaval <mukul.waval@gmail.com>
2025-12-31 23:00:15 +0530

    Revert "make m_style public"
    This reverts commit 183b17b675dbc5e14d3936a0985816196693bb92.

--- include/ImNodeFlow.h
+++ include/ImNodeFlow.h
@@ -896,8 +896,6 @@ namespace ImFlow
          * @brief <BR>Update the isSelected status of the node
          */
         void updatePublicStatus() { m_selected = m_selectedNext; }
-
-        std::shared_ptr<NodeStyle> m_style;
     private:
         NodeUID m_uid = 0;
         std::string m_title;
@@ -905,6 +903,7 @@ namespace ImFlow
         ImVec2 m_size;
         ImVec2 m_fullSize;
         ImNodeFlow* m_inf = nullptr;
+        std::shared_ptr<NodeStyle> m_style;
         bool m_selected = false, m_selectedNext = false;
         bool m_dragged = false;
         bool m_destroyed = false;
